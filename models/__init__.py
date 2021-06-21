95c1bedf06037a1fe33fd3eb93e2a99bc3b4cf1c
Nicola <occelli.nicola@gmail.com>
2021-06-21 13:49:18 +0200

    added imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,5 @@
 from .rotation import Rotation
 from .jigsaw import Jigsaw
 from .autoencoder import AutoEncoder
+from .imagenet_pretrained import ImagenetPretrained
+from .random_initalization import RandomInitialization
