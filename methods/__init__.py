7d240209d86f94d9d44d7c07653e27b16aa1644d
Nicola <occelli.nicola@gmail.com>
2021-06-21 13:46:45 +0200

    added imports

--- methods/__init__.py
+++ methods/__init__.py
@@ -1,3 +1,4 @@
 from .rotation import train_rotation, val_rotation
 from .jigsaw import train_jigsaw, val_jigsaw
 from .autoencoder import train_autoencoder, val_autoencoder
+from .supervised import train_supervised, val_supervised
