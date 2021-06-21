74a901de9c420410defe69104315d2881ffb5774
Nicola <occelli.nicola@gmail.com>
2021-06-21 13:49:03 +0200

    added model mode switching and torch nograd

--- methods/supervised.py
+++ methods/supervised.py
@@ -1,9 +1,15 @@
+import torch
 from utils.metrics import accuracy
 
 
 def train_supervised(model, img, lbl, optimizer, criterion):
+    model.train()
+
     # Zero the parameter's gradient
-    optimizer.zero_grad()
+    optimizer.zero_grad(set_to_none=True)
+
+    # Move data to GPU
+    img, lbl = img.cuda(), lbl.cuda()
 
     # Get model prediction
     out = model(img)
@@ -22,12 +28,17 @@ def train_supervised(model, img, lbl, optimizer, criterion):
 
 
 def val_supervised(model, img, lbl, criterion):
+    model.eval()
 
-    # Get model prediction
-    out = model(img)
+    with torch.no_grad():
+        # Move data to GPU
+        img, lbl = img.cuda(), lbl.cuda()
 
-    # Compute loss & metrics
-    loss = criterion(out, lbl)
-    acc = accuracy(out, lbl)
+        # Get model prediction
+        out = model(img)
+
+        # Compute loss & metrics
+        loss = criterion(out, lbl)
+        acc = accuracy(out, lbl)
 
     return loss.item(), acc.item()
