1962eac2c8397d5cb76047dc1eece01097535002
Nicola <occelli.nicola@gmail.com>
2021-06-21 13:41:30 +0200

    added parameter to choose between pretext task or downstream task

--- parser/parser.py
+++ parser/parser.py
@@ -9,11 +9,11 @@ def load_args():
     parser.add_argument('--batch_size', type=int, default=2, help='Number of images in each batch')
     parser.add_argument('--lr', type=float, default=0.025, help='learning rate used for train')
     parser.add_argument('--method', type=str, default='rotation', help='training method to use [rotation/jigsaw/...]')
+    parser.add_argument('--level', type=str, default='pretext', help='[pretext/downstream]')
     parser.add_argument('--version', type=str, default='18', help='Resnet version for the backbone [18/34]')
 
 
-
-# Data
+    # Data
     parser.add_argument('--data', type=str, default='data/kather/', help='data path')
     parser.add_argument('--rescale_size', type=int, default=72, help='size for the rescaled image')
     parser.add_argument('--crop_size', type=int, default=64, help='size of the crops')
@@ -22,6 +22,7 @@ def load_args():
     parser.add_argument('--threads', type=int, default=6, help='number of parallel threads for data loading')
 
 
+    parser.add_argument('--checkpoint_path', type=str, default='./checkpoints', help='model save root folder')
     #
     args = parser.parse_args()
 
