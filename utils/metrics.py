fixed bug in accuracy computation

utils/metrics.py
@@ -1,8 +1,8 @@
 import torch
 
 
-def accuracy(target, out):
-    pred = torch.argmax(out.data, dim=1)
+def accuracy(out, target):
+    pred = torch.argmax(out, dim=1)
     correct = torch.sum(torch.eq(pred, target))
 
     return correct / target.shape[0]