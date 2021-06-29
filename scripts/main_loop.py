083142541994bb968c25db316508e640a1225143
Nicola <occelli.nicola@gmail.com>
2021-06-29 10:53:34 +0200

    fixed accuracy bug

--- scripts/main_loop.py
+++ scripts/main_loop.py
@@ -27,10 +27,11 @@ def main_loop(args, model, train_, val_, dl_train, dl_val, optimizer, lr_schedul
         tq.set_description(f'epoch {epoch + 1}')
 
         train_running_loss = []
-        train_running_acc = []
+        train_running_corr = 0
 
         model.train()
         # Training Batch loop
+        dl_train_size = len(dl_train)
         for i, (img, lbl) in enumerate(dl_train):
             '''
             All the important training stuff can be found following the train_ function. train_ function contains the
@@ -40,49 +41,50 @@ def main_loop(args, model, train_, val_, dl_train, dl_val, optimizer, lr_schedul
             # plt.show()
             # exit()
 
-            with torch.set_grad_enabled(True):
-                loss, acc = train_(model, img, lbl, optimizer, criterion)
+            # with torch.set_grad_enabled(True):
+            loss, corr = train_(model, img, lbl, optimizer, criterion)
             # Update progress bar
             tq.update(args.train_batch_size)
-            tq.set_postfix({'train_loss': '%.6f' % loss, 'train_acc': '%.6f' % acc})
+            tq.set_postfix({'train_loss': f'{loss:6f}', 'train_acc': f'{(corr/args.train_batch_size):.6f}' })
             train_running_loss.append(loss)
-            train_running_acc.append(acc)
+            train_running_corr += corr
 
             writer.add_scalar('training_loss', train_running_loss[-1], epoch * args.train_batch_size + i)
-            writer.add_scalar('training_acc', train_running_acc[-1], epoch * args.train_batch_size + i)
+            writer.add_scalar('training_acc', corr/args.train_batch_size, epoch * args.train_batch_size + i)
 
         # Update learning rate
         lr_scheduler.step()
 
         # Close batch progress bar
         writer.add_scalar('epoch_loss', np.mean(train_running_loss), epoch)
-        writer.add_scalar('epoch_acc', np.mean(train_running_acc), epoch)
+        writer.add_scalar('epoch_acc', train_running_corr/(dl_train_size * args.train_batch_size), epoch)
 
         val_running_loss = []
-        val_running_acc = []
+        val_running_corr = 0
 
         model.eval()
         # Validation Batch Loop
+        dl_val_size = len(dl_val)
         for i, (img, lbl) in enumerate(dl_val):
             with torch.set_grad_enabled(False):
-                loss, acc = val_(model, img, lbl, criterion)
+                loss, corr = val_(model, img, lbl, criterion)
 
             val_running_loss.append(loss)
-            val_running_acc.append(acc)
+            val_running_corr += corr
 
         # Write on tensorboard
         writer.add_scalar('val_loss', np.mean(val_running_loss), epoch)
-        writer.add_scalar('val_acc', np.mean(val_running_acc), epoch)
+        writer.add_scalar('val_acc',  corr/args.val_batch_size, epoch)
 
         # Update progress bar
-        tq.set_postfix({'train_loss': '%.6f' % np.mean(train_running_loss),
-                        'train_acc': '%.6f' % np.mean(train_running_acc),
-                        'val_loss': '%.6f' % np.mean(val_running_loss),
-                        'val_acc': '%.6f' % np.mean(val_running_acc)})
+        tq.set_postfix({'train_loss': f'{np.mean(train_running_loss):.6f}',
+                        'train_acc': f'{train_running_corr/(args.train_batch_size*dl_train_size):.6f}',
+                        'val_loss': f'{np.mean(val_running_loss):.6f}',
+                        'val_acc': f'{val_running_corr/(args.val_batch_size*dl_val_size):.6f}'})
         tq.close()
 
         # Save checkpoints
-        new_acc = np.mean(val_running_acc)
+        new_acc = val_running_corr/(args.val_batch_size*dl_val_size)
         if new_acc > best_acc:
             best_acc = new_acc
             torch.save(Sequential(*list(model.backbone.children())[:-1]).state_dict(),
