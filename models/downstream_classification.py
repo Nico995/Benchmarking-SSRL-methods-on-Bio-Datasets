5b5cd263b8ce22fce8bdc3c0691e3a743b400b18
Nicola <occelli.nicola@gmail.com>
2021-06-29 10:51:35 +0200

    commenting

--- models/downstream_classification.py
+++ models/downstream_classification.py
@@ -5,24 +5,22 @@ from .resnet_backbone import get_backbone
 
 
 class DownstreamClassification(nn.Module):
-    def __init__(self, pretrained_model, num_classes, code_size=512, weights=None):
+    def __init__(self, pretrained_model, num_classes, code_size=256, weights=None):
         super(DownstreamClassification, self).__init__()
         # Use the backbone from a previous pretext training
         self.backbone = pretrained_model.backbone
+
         # Remove the last fc layer
+        # TODO: This needs to be done only for some pretext models not for everyone (i.e. autoencoder doesn't need it)
         self.backbone = nn.Sequential(
             # removing the last convolutional layer
             *list(self.backbone.children())[:-1]
         )
+
         # Freeze the backbone
         for m in self.backbone.parameters():
             m.requires_grad = False
 
-        ##############
-        # from torchsummary import summary
-        # print(summary(self.backbone.cuda(), (3, 64, 64)))
-        # exit()
-        ##############
         # Attach a linear classifier to be trained
         self.linear_head = Linear(in_features=code_size, out_features=num_classes, bias=True)
 
