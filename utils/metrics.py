0d3567190d0db531e7da28b44611ba70c7337da5
Nicola <occelli.nicola@gmail.com>
2021-06-29 10:55:12 +0200

    added the computation of number of correct prediction

--- utils/metrics.py
+++ utils/metrics.py
@@ -6,3 +6,10 @@ def accuracy(out, target):
     correct = torch.sum(torch.eq(pred, target))
 
     return correct / target.shape[0]
+
+
+def corrects(out, target):
+    pred = torch.argmax(out, dim=1)
+    correct = torch.sum(torch.eq(pred, target))
+
+    return correct
