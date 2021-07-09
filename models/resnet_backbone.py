a7a384d501a26c0b0e8c158f0e970c0ca8303188
Nicola <occelli.nicola@gmail.com>
2021-07-09 20:53:11 +0200

    added resnet50 backbone support

--- models/resnet_backbone.py
+++ models/resnet_backbone.py
@@ -1,11 +1,12 @@
 import torch
-from torchvision.models import resnet18, resnet34
+from torchvision.models import resnet18, resnet34, resnet50
 from torch.nn import Linear, Sequential
 from utils.custom_exceptions import MethodNotSupportedError
 
 resnets = {
     '18': resnet18,
     '34': resnet34,
+    '50': resnet50,
 }
 
 
@@ -17,8 +18,9 @@ def get_backbone(out_features, version='18', pretrained=False, weights=None):
     except KeyError:
         raise MethodNotSupportedError(version)
 
-    net.fc = Linear(in_features=net.fc.in_features, out_features=out_features, bias=True)
     if weights:
         net.load_state_dict(torch.load(weights), strict=False)
+
+    net.fc = Linear(in_features=net.fc.in_features, out_features=out_features, bias=True)
 
     return net
