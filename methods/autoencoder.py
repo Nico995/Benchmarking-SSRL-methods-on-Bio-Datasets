9b77ba365751997550b8e2c2504e97e1feea9b4d
Nicola <occelli.nicola@gmail.com>
2021-07-19 16:54:13 +0200

    implemented image visualization

--- methods/autoencoder.py
+++ methods/autoencoder.py
@@ -1,4 +1,25 @@
 import torch
+from matplotlib import pyplot as plt
+from torchvision.transforms import ToPILImage
+
+
+def visualize(img, out):
+    img = ToPILImage()(img[0])
+    out = ToPILImage()(out[0])
+
+    fig, ax = plt.subplots(1, 2)
+    ax = ax.ravel()
+    ax[0].imshow(img)
+    ax[1].imshow(out)
+    plt.tight_layout()
+    ax[0].axis('off')
+    ax[0].set_title('original')
+    ax[1].axis('off')
+    ax[1].set_title('encoded')
+    plt.show()
+
+
+visualized = False
 
 
 def train_autoencoder(model, img, lbl, optimizer, criterion):
@@ -10,7 +31,7 @@ def train_autoencoder(model, img, lbl, optimizer, criterion):
     out = model(img)
 
     # Compute loss & metrics
-    loss = criterion(img, out)
+    loss = criterion(out, img)
 
     # Compute parameter's gradient
     loss.backward()
@@ -20,6 +41,11 @@ def train_autoencoder(model, img, lbl, optimizer, criterion):
 
     # Optimization purposes
     loss_item = loss.item()
+
+    # temp variable needed to only visualize the first image of the validation loop
+    global visualized
+    visualized = False
+
     return loss_item, loss_item
 
 
@@ -34,6 +60,12 @@ def val_autoencoder(model, img, lbl, criterion):
 
         # Compute loss & metrics
         loss = criterion(img, out)
+
+    global visualized
+    if not visualized:
+        visualized = True
+
+        visualize(img, out)
 
     # Optimization purposes
     loss_item = loss.item()
